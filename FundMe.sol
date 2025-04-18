ef7f71fd81152bd67f53e76e297fef907b51717b
malik991 <dsci.bahawalnagar@gmail.com>
2025-04-18 14:13:11 +0500

    introduce library and for loop

--- FundMe.sol
+++ FundMe.sol
@@ -1,10 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+
+import {PriceConvertor} from "./PriceConvertor.sol"; // import library to use here
 
 contract FundMe {
      //event FundReceived(address sender, uint256 amount); // for console.log
      //event EthReceived(uint256 amount); // for console.log
+    using PriceConvertor for uint256; // attaching with uint256 hass access to all priceconvertor functions or getconversion rate function. msg.value is uint256 thats why it can call getConversionRate func.
      uint256 public myValue = 1;
      //uint256 public minimumUsd = 5;
      uint256 public minimumUsd = 5e18; // bcz value of getConversionRate(msg.value) is in EHT with 18 decimal
@@ -17,7 +19,10 @@ contract FundMe {
         myValue = myValue +2;
         // 1: how we will send ETH to this contract via payable keyword
         // require(msg.value > 1e18, "not enough ETH amount!"); //1e18 = 1ETH = 1000000000000000000 wei , require mean its complusoty
-        require(getConversionRate(msg.value) >= minimumUsd, "not enough ETH amount!"); //1e18 = 1ETH = 1000000000000000000 wei , require mean its complusoty
+        //require(getConversionRate(msg.value) >= minimumUsd, "not enough ETH amount!"); //1e18 = 1ETH = 1000000000000000000 wei , require mean its complusoty
+        require(msg.value.getConversionRate() >= minimumUsd, "not enough ETH amount!"); //getConversionRate is a function from PriceConvertor library that can convert ETH value to USD
+        // getConversionRate take a parameter 'uint ethAmount' but we didn't pass coz by default its first param is msg.value and if we want to pass 2nd param than we need to mentioned it
+        
         funders.push(msg.sender); // store address of sender to the funders array
         fundsRaisedByAddress[msg.sender] = fundsRaisedByAddress[msg.sender] + msg.value;
         //emit FundReceived(msg.sender, msg.value); // for console.log
@@ -27,32 +32,13 @@ contract FundMe {
         // for example we used a state variable myValue
     }
 
-    function getPrice() public view returns (uint256){
-        //address get from caainlink price feed for ETH '0x694AA1769357215DE4FAC081bf1f309aDC325306'
-        //ABI (get function from chainlink github repo )
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-        //get latest price data from chainlink
-        //priceFeed.latestRoundData() --> 194718520 = 36,117 ETH (Ethereum) / roundID = 1 for now
-        (
-            /* uint80 roundId */,
-            int256 price,
-            /*uint256 startedAt*/,
-            /*uint256 updatedAt*/,
-            /*uint80 answeredInRound*/
-        )= priceFeed.latestRoundData(); // becuase latestRoundData return different value so we just need price at this time
-        //*1 e18 means we want to get the usd value with unit in kilo
-        // need casting price to uint256 bcz of decimal
-        return uint256(price * 1e10); // this value is in ETH
+    function Withdraw() public  {
+        for (uint256 funderIndex; funderIndex < funders.length; funderIndex++) 
+        {
+            address funder = funders[funderIndex]; // get address from the funders Array
+            fundsRaisedByAddress[funder] = 0;   // set it to zero as we want only one time withdrawal for every funder.
+        }
     }
 
-    function getConversionRate(uint ethAmount) public view returns (uint256) {
-        uint256 ethPrice = getPrice();
-        // eth price 2000_000000000000000000
-        // we need to get eth value * usd price / usd value = ethPrice
-        // (1000000000000000000 * 2000_000000000000000000) / 1e18 = $2000 to get the value eth to usd whole number
-        // $2000 = 1 Eth
-        uint256 ethAmountInUsd = (ethAmount * ethPrice)/1e18; 
-         //emit EthReceived(ethAmountInUsd); // for console.log
-        return ethAmountInUsd;
-    }
+   
 } 
